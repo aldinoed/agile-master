fe6be1395cd2347d113a4e7abcaa1b1a697a4513
aldinoed <angga.antonov225@gmail.com>
2024-05-17 23:28:56 +0700

    kelas login bisa

--- web_project/goship/src/components/card_company.jsx
+++ web_project/goship/src/components/card_company.jsx
@@ -1,76 +1,78 @@
 import React, { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 
 const CardCompany = () => {
-  const [showMore, setShowMore] = useState(false);
-  const [data, setData] = useState([]);
+      const navigate = useNavigate()
+      const [showMore, setShowMore] = useState(false);
+      const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://goship-apii.vercel.app/api/perusahaan',{
-          headers: {
-            'Access-Control-Allow-Origin': '*'
-          }
-        });
-        const fetchedData = await response.json();
-        setData(fetchedData); // Assuming the API response directly matches the structure of the provided JSON data
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        // Handle error gracefully, e.g., display a fallback message
-      }
-    };
+      useEffect(() => {
+            const fetchData = async () => {
+                  try {
+                        const response = await fetch('https://goship-apii.vercel.app/api/perusahaan', {
+                              headers: {
+                                    'Access-Control-Allow-Origin': '*'
+                              }
+                        });
+                        const fetchedData = await response.json();
+                        setData(fetchedData); // Assuming the API response directly matches the structure of the provided JSON data
+                  } catch (error) {
+                        console.error('Error fetching data:', error);
+                        // Handle error gracefully, e.g., display a fallback message
+                  }
+            };
 
-    fetchData();
-  }, []);
+            fetchData();
+      }, []);
 
-  const handleCardClick = (id_perusahaan) => {
-  
-  };
+      const handleCardClick = (id_perusahaan) => {
+            navigate(`/detail`)
+      };
 
-  const toggleShowMore = () => {
-    setShowMore(!showMore);
-  };
+      const toggleShowMore = () => {
+            setShowMore(!showMore);
+      };
 
-  return (
-    <div className="card--container">
-      <div className="row" style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(90px, 1fr))', gap: '10px', gridRowGap: '20px' }}>
-        {data.slice(0, showMore ? data.length : 8).map((company, index) => (
-            <div key={company.id_perusahaan} >
-              <Link to="/detail">
-              <button onClick={() => handleCardClick(company.id_perusahaan)} >
-                <div className="rounded-lg overflow-hidden shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] items-center justify-center" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                    <div className="card-image ">
-                    {company.logo_perusahaan && (
-                        <img className='w-[100px] h-[100px] ss:text-[10px] rounded' src={company.logo_perusahaan} alt={company.nama_perusahaan} />
-                    )}
-                    {!company.logo_perusahaan && (
-                        <div className="placeholder-icon">
-                        <i className="fas fa-image-not-found"></i>
-                        </div>
-                    )}                   
-                    </div>              
-                </div>
-                <div className="card-title ss:text-[12px] font-bold py-2">{company.nama_perusahaan}</div>
-                </button>
-                </Link>
+      return (
+            <div className="card--container">
+                  <div className="row" style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(90px, 1fr))', gap: '10px', gridRowGap: '20px' }}>
+                        {data.slice(0, showMore ? data.length : 8).map((company, index) => (
+                              <div key={company.id_perusahaan} >
+                                    <Link to="/detail" state={company.id}>
+                                          <button onClick={() => handleCardClick(company.id_perusahaan)} >
+                                                <div className="rounded-lg overflow-hidden shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] items-center justify-center" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                                      <div className="card-image ">
+                                                            {company.logo_perusahaan && (
+                                                                  <img className='w-[100px] h-[100px] ss:text-[10px] rounded' src={company.logo_perusahaan} alt={company.nama_perusahaan} />
+                                                            )}
+                                                            {!company.logo_perusahaan && (
+                                                                  <div className="placeholder-icon">
+                                                                        <i className="fas fa-image-not-found"></i>
+                                                                  </div>
+                                                            )}
+                                                      </div>
+                                                </div>
+                                                <div className="card-title ss:text-[12px] font-bold py-2">{company.nama_perusahaan}</div>
+                                          </button>
+                                    </Link>
+                              </div>
+                        ))}
+                        {data.length > 9 && (
+                              <div key="show-more" className="card ss:text-[30px] font-bold">
+                                    <div className='md:h-[100px] h-max-[1200px] rounded-lg overflow-hidden shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] flex items-center justify-center '>
+                                          <div className='px-3 outline rounded-lg'>
+                                                <button className='' onClick={toggleShowMore}>{showMore ? '-' : '+'}</button>
+
+                                          </div>
+                                    </div>
+                              </div>
+                        )}
+                  </div>
             </div>
-        ))}
-        {data.length > 9 && (
-        <div key="show-more" className="card ss:text-[30px] font-bold">
-            <div className='md:h-[100px] h-max-[1200px] rounded-lg overflow-hidden shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] flex items-center justify-center '>
-                <div className='px-3 outline rounded-lg'>
-                    <button className=''  onClick={toggleShowMore}>{showMore  ? '-' : '+' }</button>
-                    
-                </div>
-            </div>
-        </div>
-        )}
-      </div>
-    </div>
-  );
+      );
 };
 
 export default CardCompany;
--- web_project/goship/src/pages/Login.jsx
+++ web_project/goship/src/pages/Login.jsx
@@ -9,119 +9,121 @@ let storage = require('../storage')
 // import { putHistory, showHistory } from "../storage";
 
 const LoginPage = () => {
-  let refreshToken = Cookies.get('refresh_token')
-  const navigate = useNavigate();
-  
+      let refreshToken = Cookies.get('refresh_token')
+      const navigate = useNavigate();
 
-  const handleUserClick = () => {
-    // Navigasi ke halaman Forgot_Password_User saat tombol "User" diklik
-    navigate("/forgot-password-user");
-  };
 
-  const handleForgotPasswordClick = () => {
-    // Navigasi ke halaman Forgot_Password_User saat tombol "Forgot password?" diklik
-    navigate("/forgot-password-user");
-  };
-  const [nrp, setnrp] = useState('');
-  const [password, setPassword] = useState('');
+      const handleUserClick = () => {
+            // Navigasi ke halaman Forgot_Password_User saat tombol "User" diklik
+            navigate("/forgot-password-user");
+      };
 
-  const [formDataAdmin, setFormDataAdmin] = useState({
-    email: "",
-    password: "",
-  });
+      const handleForgotPasswordClick = () => {
+            // Navigasi ke halaman Forgot_Password_User saat tombol "Forgot password?" diklik
+            navigate("/forgot-password-user");
+      };
+      const [nrp, setnrp] = useState('');
+      const [password, setPassword] = useState('');
 
-  useEffect(()=>{
-    if(refreshToken){
-      navigate('/');
-    }
-  })
+      const [formDataAdmin, setFormDataAdmin] = useState({
+            email: "",
+            password: "",
+      });
 
-  const [isUserHovered, setIsUserHovered] = useState(false);
-  const [isAdminHovered, setIsAdminHovered] = useState(false);
-  const [isAdminForm, setIsAdminForm] = useState(false); // State untuk menentukan apakah form untuk admin sedang ditampilkan
+      useEffect(() => {
+            if (refreshToken) {
+                  navigate('/');
+            }
+      })
 
-  // const handleChangeUser = (e) => {
-  //   setFormDataUser({ ...formDataUser, [e.target.name]: e.target.value });
-  // };
+      const [isUserHovered, setIsUserHovered] = useState(false);
+      const [isAdminHovered, setIsAdminHovered] = useState(false);
+      const [isAdminForm, setIsAdminForm] = useState(false); // State untuk menentukan apakah form untuk admin sedang ditampilkan
 
-  const handleChangeAdmin = (e) => {
-    setFormDataAdmin({ ...formDataAdmin, [e.target.name]: e.target.value });
-  };
+      // const handleChangeUser = (e) => {
+      //   setFormDataUser({ ...formDataUser, [e.target.name]: e.target.value });
+      // };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+      const handleChangeAdmin = (e) => {
+            setFormDataAdmin({ ...formDataAdmin, [e.target.name]: e.target.value });
+      };
 
-    const credential = {
-      nrp: nrp,
-      password: password
-    }
-    try {
-      const response = await axios.post('https://goship-apii.vercel.app/auth/login', credential);
-      Cookies.set('refresh_token', response.data.token, {expires : 10 / (24 * 60)})
-            
-      if (response.data.user.is_first_auth === 1) {
-        Swal.fire({
-          title: 'Behasil!',
-          text: response.data.message,
-          icon: "success",
+      const handleSubmit = async (e) => {
+            e.preventDefault();
 
-        })
-        storage.set({key : response.data.user})
-        navigate('/reset-password-user', { state: response.data.user  })
-      }else{
-        Swal.fire({
-          title: 'Berhasil!',
-          text: response.data.message,
-          icon: "success",
-        })
-        localStorage.setItem('id', response.data.user.id_siswa)
-        localStorage.setItem('nama', response.data.user.nama_siswa)
-        localStorage.setItem('nrp', response.data.user.nrp)
-        const authorizationKey = {
-          user: response.data.user,
-          token : response.data.token
-        }
-        navigate('/', {state: authorizationKey})
-      }
-    } catch (error) {
-      console.log(error)
-    }
-    // if (isAdminForm) {
-    //   console.log(formDataAdmin);
-    // } else {
-    //   console.log(formDataUser);
-    // }
-  };
+            const credential = {
+                  nrp: nrp,
+                  password: password
+            }
+            try {
+                  const response = await axios.post('https://goship-apii.vercel.app/auth/login', credential);
+                  Cookies.set('refresh_token', response.data.token, { expires: 10 / (24 * 60) })
 
-  const handleAdminClick = () => {
-    setIsAdminForm(true); // Menampilkan form untuk admin
-  };
+                  if (response.data.user.is_first_auth === 1) {
+                        Swal.fire({
+                              title: 'Behasil!',
+                              text: response.data.message,
+                              icon: "success",
 
-  const handleUserButtonClick = () => {
-    setIsAdminForm(false);
-    // handleUserClick();
-  };
-  return (
-    <div
-      className="flex justify-center items-center h-screen relative"
-      style={{
-        backgroundImage: `url(${image21})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="bg-white rounded-lg shadow-md p-8 max-w-3xl z-10">
-        <div className="mt-2 flex justify-center items-center">
-          <img
-            src={login}
-            alt="Gambar Login"
-            className="mr-5"
-            style={{ width: "300px", height: "300px" }}
-          />
-          <div>
-            <div className="flex justify-end mb-8">
-              <div className="flex">
-                {/* <button
+                        })
+                        localStorage.setItem('id', response.data.user.id_siswa)
+                        localStorage.setItem('nama', response.data.user.nama_siswa)
+                        localStorage.setItem('nrp', response.data.user.nrp)
+                        navigate('/reset-password-user', { state: response.data.user })
+                  } else {
+                        Swal.fire({
+                              title: 'Berhasil!',
+                              text: response.data.message,
+                              icon: "success",
+                        })
+                        localStorage.setItem('id', response.data.user.id_siswa)
+                        localStorage.setItem('nama', response.data.user.nama_siswa)
+                        localStorage.setItem('nrp', response.data.user.nrp)
+                        const authorizationKey = {
+                              user: response.data.user,
+                              token: response.data.token
+                        }
+                        navigate('/', { state: authorizationKey })
+                  }
+            } catch (error) {
+                  console.log(error)
+            }
+            // if (isAdminForm) {
+            //   console.log(formDataAdmin);
+            // } else {
+            //   console.log(formDataUser);
+            // }
+      };
+
+      const handleAdminClick = () => {
+            setIsAdminForm(true); // Menampilkan form untuk admin
+      };
+
+      const handleUserButtonClick = () => {
+            setIsAdminForm(false);
+            // handleUserClick();
+      };
+      return (
+            <div
+                  className="flex justify-center items-center h-screen relative"
+                  style={{
+                        backgroundImage: `url(${image21})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                  }}
+            >
+                  <div className="bg-white rounded-lg shadow-md p-8 max-w-3xl z-10">
+                        <div className="mt-2 flex justify-center items-center">
+                              <img
+                                    src={login}
+                                    alt="Gambar Login"
+                                    className="mr-5"
+                                    style={{ width: "300px", height: "300px" }}
+                              />
+                              <div>
+                                    <div className="flex justify-end mb-8">
+                                          <div className="flex">
+                                                {/* <button
                   className="bg-orange-500 text-white font-bold py-2 px-4 rounded mr-2 border border-orange-500"
                   style={{
                     backgroundColor:
@@ -135,7 +137,7 @@ const LoginPage = () => {
                 >
                   User
                 </button> */}
-                {/* <button
+                                                {/* <button
                   className="bg-orange-500 text-white font-bold py-2 px-4 rounded border border-orange-500"
                   style={{
                     backgroundColor:
@@ -149,64 +151,64 @@ const LoginPage = () => {
                 >
                   Admin
                 </button> */}
-              </div>
-            </div>
-            <div>
-              <h2 className="text-2xl font-bold mb-2">
-                Hei, dari mana saja kamu?
-              </h2>
-              <h6 className="text-xs mb-1 text-gray-500">
-                Login untuk mengakses kembali fitur GoShip
-              </h6>
-            </div>
-            <form onSubmit={handleSubmit}>
-              <div className="border-b border-gray-900/10 pb-12">
-                {!isAdminForm && (
-                  <>
-                    <div className="mt-6 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-6">
-                      <div className="sm:col-span-6">
-                        <div className="input-group flex border border-1 rounded-md w-full">
-                          <img
-                            src={orang}
-                            alt="profile"
-                            className="input-icon m-2 h-5 w-5"
-                          />
-                          <input
-                            type="number"
-                            name="nrp"
-                            id="nrp"
-                            placeholder="NRP"
-                            value={nrp}
-                            onChange={(e) => setnrp(e.target.value)}
-                            className="flex-grow py-1.5 px-3 text-gray-700 placeholder-gray-400 placeholder-xs focus:ring-inset focus:ring-indigo-600 sm:text-sm border-0"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    <div className="mt-6 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-6">
-                      <div className="sm:col-span-6">
-                        <div className="input-group flex border border-1 rounded-md w-full">
-                          <img
-                            src={gembok}
-                            alt="password"
-                            className="input-icon m-2 h-5 w-5"
-                          />
-                          <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            placeholder="Masukkan Kata Sandi Anda"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="flex-grow py-1.5 px-3 text-gray-700 placeholder-gray-400 placeholder-xs focus:ring-inset focus:ring-indigo-600 sm:text-sm border-0"
-                            required
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </>
-                )}
-                {/* {isAdminForm && ( // Menampilkan form untuk admin jika isAdminForm true
+                                          </div>
+                                    </div>
+                                    <div>
+                                          <h2 className="text-2xl font-bold mb-2">
+                                                Hei, dari mana saja kamu?
+                                          </h2>
+                                          <h6 className="text-xs mb-1 text-gray-500">
+                                                Login untuk mengakses kembali fitur GoShip
+                                          </h6>
+                                    </div>
+                                    <form onSubmit={handleSubmit}>
+                                          <div className="border-b border-gray-900/10 pb-12">
+                                                {!isAdminForm && (
+                                                      <>
+                                                            <div className="mt-6 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-6">
+                                                                  <div className="sm:col-span-6">
+                                                                        <div className="input-group flex border border-1 rounded-md w-full">
+                                                                              <img
+                                                                                    src={orang}
+                                                                                    alt="profile"
+                                                                                    className="input-icon m-2 h-5 w-5"
+                                                                              />
+                                                                              <input
+                                                                                    type="number"
+                                                                                    name="nrp"
+                                                                                    id="nrp"
+                                                                                    placeholder="NRP"
+                                                                                    value={nrp}
+                                                                                    onChange={(e) => setnrp(e.target.value)}
+                                                                                    className="flex-grow py-1.5 px-3 text-gray-700 placeholder-gray-400 placeholder-xs focus:ring-inset focus:ring-indigo-600 sm:text-sm border-0"
+                                                                              />
+                                                                        </div>
+                                                                  </div>
+                                                            </div>
+                                                            <div className="mt-6 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-6">
+                                                                  <div className="sm:col-span-6">
+                                                                        <div className="input-group flex border border-1 rounded-md w-full">
+                                                                              <img
+                                                                                    src={gembok}
+                                                                                    alt="password"
+                                                                                    className="input-icon m-2 h-5 w-5"
+                                                                              />
+                                                                              <input
+                                                                                    type="password"
+                                                                                    id="password"
+                                                                                    name="password"
+                                                                                    placeholder="Masukkan Kata Sandi Anda"
+                                                                                    value={password}
+                                                                                    onChange={(e) => setPassword(e.target.value)}
+                                                                                    className="flex-grow py-1.5 px-3 text-gray-700 placeholder-gray-400 placeholder-xs focus:ring-inset focus:ring-indigo-600 sm:text-sm border-0"
+                                                                                    required
+                                                                              />
+                                                                        </div>
+                                                                  </div>
+                                                            </div>
+                                                      </>
+                                                )}
+                                                {/* {isAdminForm && ( // Menampilkan form untuk admin jika isAdminForm true
                   <div>
                     <div className="mt-6 grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-6">
                       <div className="sm:col-span-6">
@@ -250,7 +252,7 @@ const LoginPage = () => {
                     </div>
                   </div>
                 )} */}
-                {/* <div className="flex justify-end mt-2">
+                                                {/* <div className="flex justify-end mt-2">
                   <a
                     href={
                       isAdminForm
@@ -263,22 +265,22 @@ const LoginPage = () => {
                     Tidak ingat kata sandi?
                   </a>
                 </div> */}
-              </div>
-              <div className="flex flex-col" style={{ marginTop: "-25px" }}>
-                <button
-                  type="submit"
-                  className="text-white font-bold py-2 px-2 rounded mb-1"
-                  style={{ backgroundColor: "#F77D00" }}
-                >
-                  Masuk
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+                                          </div>
+                                          <div className="flex flex-col" style={{ marginTop: "-25px" }}>
+                                                <button
+                                                      type="submit"
+                                                      className="text-white font-bold py-2 px-2 rounded mb-1"
+                                                      style={{ backgroundColor: "#F77D00" }}
+                                                >
+                                                      Masuk
+                                                </button>
+                                          </div>
+                                    </form>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      );
 };
 
 export default LoginPage;
