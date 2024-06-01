4ed37428cec86bc5a32267701411757d8bb9da9e
ahmaddwiki <114077731+ahmaddwiki@users.noreply.github.com>
2024-06-01 13:57:01 +0700

    handling Add & Edit Dshboard Admin beserta Logic Handling Add & Edit Dashboard Admin

---
+++ web_project/goship/src/components/addfailed.jsx
@@ -0,0 +1,62 @@
+import { logo1, failed } from "../assets";
+import React from "react";
+
+const CardAddFailed = ({ onClose }) => {
+  // State dan logika lainnya di sini...
+
+  return (
+    <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-gray-500 bg-opacity-50 z-50 overflow-auto py-3 ">
+      <div
+        className="bg-white py-6 px-10 rounded-lg shadow-md max-w-full max-h-full overflow-y-auto overflow-y-scroll no-scrollbar"
+        style={{ width: "800px", height: "500px" }}
+      >
+        <div className="flex items-center justify-between mb-4">
+          <div className="flex items-center">
+            <img src={logo1} alt="Goship Logo" className="h-10 w-auto mr-4" />
+            <div className="border-l-2 border-black pl-4">
+              <h2 className="text-lg font-semibold">Notifikasi Gagal</h2>
+            </div>
+          </div>
+          <button
+            onClick={onClose}
+            className="text-gray-500 hover:text-gray-700 focus:outline-none border-2 border-black rounded-md p-1"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div className="mt-4 flex items-center justify-center">
+          <img
+            src={failed} // Ganti yourImageHere dengan sumber gambar Anda
+            alt="Maaf, anda gagal menambahkan data perusahaan"
+            style={{ width: "325px", height: "300px" }}
+          />
+        </div>
+
+        <div className="text-center mt-4">
+          <p className="text-black">
+            Maaf, anda{" "}
+            <span style={{ color: "#F77D00" }}>Gagal untuk menambahkan </span>{" "}
+            data perusahaan
+          </p>
+          <p className="font-normal"> Tolong, coba sekali lagi!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardAddFailed;
