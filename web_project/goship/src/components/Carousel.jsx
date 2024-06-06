13a27afb2ad1ed396b73c8b8ae76df96db7181b1
aldinoed <angga.antonov225@gmail.com>
2024-06-06 17:53:52 +0700

    disable all ctrl shift and right click

--- web_project/goship/src/components/Carousel.jsx
+++ web_project/goship/src/components/Carousel.jsx
@@ -2,23 +2,9 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 import StudentList from "./StudentList";
 import styles from "../style";
-import React, { useEffect } from 'react';
 
 const CustomCarousel = () => {
-      document.addEventListener('contextmenu', event => event.preventDefault());
-      useEffect(() => {
-            const handleKeyDown = (event) => {
-                  if (event.ctrlKey || event.shiftKey) {
-                        event.preventDefault();
-                  }
-            };
 
-            document.addEventListener('keydown', handleKeyDown);
-
-            return () => {
-                  document.removeEventListener('keydown', handleKeyDown);
-            };
-      }, []);
       return (
             <>
                   <section id="Our_story" className={`flex flex-col md:flex-row`}>
